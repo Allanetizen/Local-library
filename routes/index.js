eeac07ffc44594b5c0c730710adc1092313a6c7f
Allan <allankosgei747@gmail.com>
2022-02-26 01:32:30 +0300

    req,res params and packages update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 //next makes the function below a middleware function 
 //can be used to add multiple route handlers to the '/' route path
-router.get('/', (req, res, )=> {
+router.get('/', (req, res)=> {
   res.redirect('/catalog');
 });
 
